288b6c06ce5815f178ef049148ab08d54224a6c7
* <you@example.com>
2024-01-27 16:44:48 +0100

    Done with some final touches

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ const darkBtn = document.getElementById('dark')
 const lightBtn = document.getElementById('light')
 const orderBody = document.getElementById("orderBody")
 
+if(document.cookie == "theme=darkmode"){
+    darkTheme()
+}
+else if(document.cookie == "theme=lightmode"){
+    lightTheme()
+}
+
 darkBtn.addEventListener('click', ()=>{
+    document.cookie = "theme=darkmode; expires=Mon, 21 April 2099 12:00:00"
+    darkTheme()
+})
+lightBtn.addEventListener('click', ()=>{
+    document.cookie = "theme=lightmode; expires=Mon, 21 April 2099 12:00:00"
+    lightTheme()
+})
+
+function darkTheme(){
+document.querySelector('.headerDiv2').classList.add('darkmode2')
     document.body.classList.add('darkmode')
     document.querySelector('.themeBtnContainer').classList.add('darkmode')
     document.querySelector('header').classList.add('darkmode')
@@ -14,9 +31,10 @@ darkBtn.addEventListener('click', ()=>{
     document.querySelector('.orderGraphs').classList.add('darkmode3')
     lightBtn.style.background = "none"
     darkBtn.style.background = "#34CAA5"
-})
+}
 
-lightBtn.addEventListener('click', ()=>{
+function lightTheme(){
+document.querySelector('.headerDiv2').classList.remove('darkmode2')
     document.querySelector('.themeBtnContainer').classList.remove('darkmode')
     document.body.classList.remove('darkmode')
     document.querySelector('.barChart').classList.remove('darkmode2')
@@ -27,16 +45,18 @@ lightBtn.addEventListener('click', ()=>{
     document.querySelector('header').classList.remove('darkmode')
     lightBtn.style.background = "#34CAA5"
     darkBtn.style.background = "none"
-})
+}
 
 document.querySelector('.menuBtn').addEventListener('click',()=>{
     document.querySelector('nav').style.display = 'flex'
     document.querySelector('.closeBtn').style.display = 'block'
+    document.querySelector('.headerDiv2').style.display = 'flex'
 })
 
 document.querySelector('.closeBtn').addEventListener('click',()=>{
     document.querySelector('nav').style.display = 'none'
     document.querySelector('.closeBtn').style.display = 'none'
+    document.querySelector('.headerDiv2').style.display = 'none'
 })
 
 const platforms = [
